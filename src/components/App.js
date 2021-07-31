10190b461a80265efb30d107e6f22aef68c76b2f
M Hughes <mhughes2011@gmail.com>
2021-07-30 17:24:49 -0700

    implemented 404 routing with Switch

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 //App components
 import Header from './Header';
@@ -7,15 +7,21 @@ import Home from './Home';
 import About from './About';
 import Teachers from './Teachers';
 import Courses from './Courses';
+import NotFound from './NotFound';
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
       <Header />
-      <Route exact path="/" component={Home} />
-      <Route path="/about" render={ () => <About title='About' /> } /> {/* This is how you pass props to components via routes. */}
-      <Route path='/teachers' component={Teachers} />
-      <Route path='/courses' component={Courses} />
+
+      {/* The Switch statement renders the first route that matches the url otherwise the catch all is the bottom one with no path */}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" render={ () => <About title='About' /> } /> {/* This is how you pass props to components via routes. */}
+        <Route path='/teachers' component={Teachers} />
+        <Route path='/courses' component={Courses} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
